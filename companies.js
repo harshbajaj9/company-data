fc7cf998e125552af072a37965a4ec9ef0acbb07
Harsh Bajaj <42932991+harshbajaj9@users.noreply.github.com>
2024-12-15 14:03:24 +0530

    Update processedCompanies.js with new entries

--- companies.js
+++ companies.js
@@ -8,5 +8,10 @@ const parsedCompanies = [
     "name": "devroad",
     "domain": "https://devroad.io",
     "boards": []
+  },
+  {
+    "name": "devroad1",
+    "domain": "https://devroad.io",
+    "boards": []
   }
 ];
