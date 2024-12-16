f371f0b3534ad94f56c3de1874b31a9e27c659b7
Harsh Bajaj <42932991+harshbajaj9@users.noreply.github.com>
2024-12-16 20:48:24 +0530

    Update companies.js with Google

--- companies.js
+++ companies.js
@@ -5,12 +5,16 @@ const parsedCompanies = [
     "domain": "https://www.google.com/about/careers/applications/jobs/results",
     "boards": [
       {
-        "url": "https://www.google.com/about/careers/applications/jobs/results",
-        "title": "Google Careers"
+        "title": "Google Careers",
+        "url": "https://www.google.com/about/careers/applications/jobs/results"
       },
       {
-        "url": "https://www.instahyre.com/jobs-at-google-2/",
-        "title": "instahyre"
+        "title": "instahyre",
+        "url": "https://www.instahyre.com/jobs-at-google-2/"
+      },
+      {
+        "title": "LinkedIn",
+        "url": "https://www.linkedin.com/company/google/jobs"
       }
     ]
   }
