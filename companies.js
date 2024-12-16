02d683e3b0b062ee6b366c3152f1997252df5c04
Harsh Bajaj <42932991+harshbajaj9@users.noreply.github.com>
2024-12-16 20:51:29 +0530

    Update companies.js with Google

--- companies.js
+++ companies.js
@@ -15,6 +15,22 @@ const parsedCompanies = [
       {
         "title": "LinkedIn",
         "url": "https://www.linkedin.com/company/google/jobs"
+      },
+      {
+        "title": "Indeed",
+        "url": "https://www.indeed.com/cmp/Google/jobs"
+      },
+      {
+        "title": "GLASSDOOR",
+        "url": "https://www.glassdoor.com/Jobs/Google-Jobs-E9079.htm"
+      },
+      {
+        "title": "naukri",
+        "url": "https://www.naukri.com/google-jobs"
+      },
+      {
+        "title": "X",
+        "url": "https://x.com/jobs?cname=google"
       }
     ]
   }
